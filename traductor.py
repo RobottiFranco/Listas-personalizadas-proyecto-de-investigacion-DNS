48bf92302a02405b6d4518fa0d797bad110415fe
RobottiFranco <franco.robotti0@gmail.com>
2024-11-08 15:02:24 -0300

    funcion unificada

--- traductor.py
+++ traductor.py
@@ -1,27 +1,17 @@
 import csv
 
-""" funcion que toma un archivo .csv de OONI y lo transforma para su utilziacion de OONI run link"""
-def traductorOONI(archivo, outName):
+""" funcion que toma un archivo .csv y lo transforma para su utilizacion de OONI run link"""
+""" toma el archivo original y el nombre del archivo de salida, y la columna que se desea traducir"""
+def traductor(archivo, outName, columna):
     with open(archivo, "r", newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
         with open(outName, "w", newline='', encoding='utf-8') as outfile:
             writer = csv.writer(outfile)
             for row in reader:
-                    writer.writerow([row[0].strip()])
-                    print(row[0].strip())
+                if row[columna].strip() != "":
+                    writer.writerow([row[columna].strip()])
+                    print(row[columna].strip())
 
-traductorOONI("ooniList\\ve.csv", "ooniList\\ve_run_link.csv")
-traductorOONI("ooniList\\uy.csv", "ooniList\\uy_run_link.csv")
-
-""" funcion que toma un archivo .csv de ipsvenezuela y lo transforma para su utilziacion de OONI run link"""
-def traductorIpysvenezuela(archivo, outName):
-    with open(archivo, "r", newline='', encoding='utf-8') as csvfile:
-        reader = csv.reader(csvfile)
-        with open(outName, "w", newline='', encoding='utf-8') as outfile:
-            writer = csv.writer(outfile)
-            for row in reader:
-                    writer.writerow([row[0].strip()])
-                    print(row[0].strip())
-
-traductorIpysvenezuela("ipysvenezuela.org\\ve.csv", "ipysvenezuela.org\\ve_run_link.csv")
-
+traductor("ooniList\\ve.csv", "ooniList\\ve_run_link.csv", 0)
+traductor("ooniList\\uy.csv", "ooniList\\uy_run_link.csv", 0)
+traductor("ipysvenezuela.org\\ve.csv", "ipysvenezuela.org\\ve_run_link.csv", 1)

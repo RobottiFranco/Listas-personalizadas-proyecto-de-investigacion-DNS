99c71b69237c6fca976efd8ebb90166202aa19da
RobottiFranco <franco.robotti0@gmail.com>
2024-11-18 02:00:58 -0300

    fix format

--- traductor.py
+++ traductor.py
@@ -2,16 +2,17 @@ import csv
 
 """ funcion que toma un archivo .csv y lo transforma para su utilizacion de OONI run link"""
 """ toma el archivo original y el nombre del archivo de salida, y la columna que se desea traducir"""
-def traductor(archivo, outName, columna):
+def traductor(archivo, outName, columna, delimitador=','):
     with open(archivo, "r", newline='', encoding='utf-8') as csvfile:
-        reader = csv.reader(csvfile)
+        reader = csv.reader(csvfile, delimiter=delimitador)
         with open(outName, "w", newline='', encoding='utf-8') as outfile:
             writer = csv.writer(outfile)
             for row in reader:
-                if row[columna].strip() != "":
-                    writer.writerow([row[columna].strip()])
-                    print(row[columna].strip())
+                try:
+                    if row[columna].strip() != "":
+                        writer.writerow([row[columna].strip()])
+                except IndexError:
+                    print(f"Elemento saltado: {row}")
 
-traductor("ooniList\\ve.csv", "ooniList\\ve_run_link.csv", 0)
-traductor("ooniList\\uy.csv", "ooniList\\uy_run_link.csv", 0)
-traductor("ipysvenezuela.org\\ve.csv", "ipysvenezuela.org\\ve_run_link.csv", 1)
+traductor("uruguay\\uy-citizenlab.csv", "uruguay\\uy-citizenlab-clear.csv", 0, delimitador=',')
+traductor("uruguay\\uy-2023-2024.csv", "uruguay\\uy-2023-2024-clear.csv", 1, delimitador=';')

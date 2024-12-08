f3d87c2a7e3f5b3fb318221ab8a2f4dcc3215681
RobottiFranco <franco.robotti0@gmail.com>
2024-12-08 18:11:53 -0300

    graficos fix

--- graficarAnualidadPorPais.py
+++ graficarAnualidadPorPais.py
@@ -3,17 +3,22 @@ from Clases.helper.calcularRangoFechaMes import obtener_rango_mes
 from Clases.helper.globalVariables import consulta_grafica as base_url, diccionario_Paise_lista_ooni_historica
 from Clases.helper.obtenerDatosOoniDB import obtener_datos_ooni_db
 from Clases.Consulta import Consulta
-from Clases.helper.obtenerGrafico import obtener_grafico
+from Clases.helper.obtenerGrafico import obtener_grafico, obtener_grafico_mejorado
 
 
-def graficar_anualidad_por_pais(since: int, until: int) -> None:
+def graficar_anualidad_por_pais(since: int, until: int, mejorado: bool = False) -> None:
     for pais in diccionario_Paise_lista_ooni_historica:
+        init = since
         while since <= until:
-            consulta = Consulta(base_url, "web_connectivity", pais, f"{since}-01-01", f"{since}-12-31", None, None, None, None, None)
-            obtener_grafico(consulta, "month", "category_code", None, f"Graficos/anuales/{pais}", since)
+            consulta = Consulta(base_url, "web_connectivity", pais, f"{init}-01-01", f"{since}-12-31", None, None, None, None, None)
+            if mejorado:
+                obtener_grafico_mejorado(consulta, "month", "category_code", None, f"Graficos/anuales/mejorados/{pais}", since)
+            else:
+                obtener_grafico(consulta, "month", "category_code", None, f"Graficos/anuales/nativos/{pais}", since)
             since += 1
         since = 2016
 
 # Main
 
 graficar_anualidad_por_pais(2016, 2024)
+graficar_anualidad_por_pais(2016, 2024, True)
--- graficarPaisesSinVPN.py
+++ graficarPaisesSinVPN.py
@@ -6,18 +6,18 @@ def graficar_OONI_explorer(mejorada: bool = False):
     for pais in diccionario_Paise_lista_ooni_historica:
         consulta = Consulta(base_url, "web_connectivity", pais, "2016-01-01", "2024-12-31", None, None, None, None, None)
         if mejorada:
-            obtener_grafico_mejorado(consulta, "month", "category_code", None, "Graficos/mejorados/ooni_explorer", f"{pais} OONI Explorer (todo tipo de bloqueos)")
+            obtener_grafico_mejorado(consulta, "month", "category_code", None, "Graficos/listas/mejorados/ooni_explorer", f"{pais} OONI Explorer (todo tipo de bloqueos)")
         else:
-            obtener_grafico(consulta, "month", "category_code", None, "Graficos/nativos/ooni_explorer", f"{pais} OONI Explorer (todo tipo de bloqueos)")
+            obtener_grafico(consulta, "month", "category_code", None, "Graficos/listas/nativos/ooni_explorer", f"{pais} OONI Explorer (todo tipo de bloqueos)")
 
 
 def graficar_lista_historica(mejorada: bool = False):
     for pais in diccionario_Pais_lista_ooni_actualizada:
         consulta = Consulta(base_url, "web_connectivity", pais, "2024-01-01", "2025-12-31", diccionario_Paise_lista_ooni_historica[pais], None, None, None, None)
         if mejorada:
-            obtener_grafico_mejorado(consulta, "month", "category_code", None, "Graficos/mejorados/historico", f"{pais} historica sin VPN (todo tipo de bloqueos)")
+            obtener_grafico_mejorado(consulta, "month", "category_code", None, "Graficos/listas/mejorados/historico", f"{pais} historica sin VPN (todo tipo de bloqueos)")
         else:
-            obtener_grafico(consulta, "month", "category_code", None, "Graficos/nativos/historico", f"{pais} historica sin VPN (todo tipo de bloqueos)")
+            obtener_grafico(consulta, "month", "category_code", None, "Graficos/listas/nativos/historico", f"{pais} historica sin VPN (todo tipo de bloqueos)")
 
    
         
@@ -25,9 +25,9 @@ def graficar_lista_actualizada(mejorada: bool = False):
     for pais in diccionario_Pais_lista_ooni_actualizada:
         consulta = Consulta(base_url, "web_connectivity", pais, "2024-01-01", "2025-12-31", diccionario_Pais_lista_ooni_actualizada[pais], None, None, None, None)
         if mejorada:
-            obtener_grafico_mejorado(consulta, "month", "category_code", None, "Graficos/mejorados/actualizado", f"{pais} actualizada sin VPN (solo DNS)")
+            obtener_grafico_mejorado(consulta, "month", "category_code", None, "Graficos/listas/mejorados/actualizado", f"{pais} actualizada sin VPN (solo DNS)")
         else:
-            obtener_grafico(consulta, "month", "category_code", None, "Graficos/nativos/actualizado", f"{pais} actualizada sin VPN (solo DNS)")
+            obtener_grafico(consulta, "month", "category_code", None, "Graficos/listas/nativos/actualizado", f"{pais} actualizada sin VPN (solo DNS)")
 
 
 # Main
